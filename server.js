3cc48490c7e14d7e451ccd88a53a27edd362739a
denis gariglio <denisgariglio@gmail.com>
2019-12-27 22:24:48 -0300

    iniciando projeto

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const http = require('http');
+
+const servidor = http.createServer(function(req, resp){
+    
+    let html = '';
+    if(req.url == '/'){
+        html = `<html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Denis Gariglio </h1>
+                    </body> 
+                </html>`;
+    } else if (req.url == '/livros') {
+        html = `<html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Listagem de livros </h1>
+                    </body> 
+                </html>`;
+    }
+    resp.end(html)
+    });
+servidor.listen(3000);
