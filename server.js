fc6e1f9be941e4d76d93ab2e64a35a14dba5e234
denis gariglio <denisgariglio@gmail.com>
2020-01-16 22:48:54 -0300

    incluindo db

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000');
 });
 
-app.get('/', function(req, resp){
-    resp.send(`<html>
-                         <head>
-                             <meta charset="utf-8">
-                         </head>
-                         <body>
-                             <h1> Denis Gariglio </h1>
-                         </body> 
-                     </html>`);
-});
 
-app.get('/livros', function(req, resp){
-    resp.send(`<html>
-                         <head>
-                             <meta charset="utf-8">
-                         </head>
-                         <body>
-                             <h1> Listagem de livros </h1>
-                         </body> 
-                     </html>`);
-});
